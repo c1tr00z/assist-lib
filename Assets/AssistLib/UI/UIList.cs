63ef38310e8b50b99bb2871a15193ddf6c9b2ef7
c1tr00z <c1tr00z@yandex.ru>
2019-04-17 13:50:01 +0300

    new, more simple ui list

--- Assets/AssistLib/UI/UIList.cs
+++ Assets/AssistLib/UI/UIList.cs
@@ -3,43 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(ScrollRect))]
+[RequireComponent(typeof(LayoutGroup))]
 public class UIList : MonoBehaviour {
-
-    public enum StartPoint {
-        Center,
-        Top,
-        Bottom,
-        Left,
-        Right,
-        //TopLeft,
-        //TopRight,
-        //BottomLeft,
-        //BottomRight,
-    }
-
-    [SerializeField] private ScrollRect _scrollRect;
-
-    [SerializeField] private StartPoint _listStartPoint;
 
     [SerializeField] private UIListItem listItemSource;
 
-    [SerializeField] private Vector2 _cellSize;
-
-    [SerializeField] private Vector2 _gridSize;
-
-    [SerializeField] private RectTransform _contentTransform;
-
     private List<UIListItem> _listItems;
 
-    void Awake() {
-        if (_scrollRect == null) {
-            _scrollRect = GetComponent<ScrollRect>();
-        }
-    }
-
     public void UpdateList<T>(IEnumerable<T> items) {
-        _contentTransform = _scrollRect.content.transform as RectTransform;
         if (_listItems != null) {
             foreach (var listItem in _listItems) {
                 Destroy(listItem.gameObject);
@@ -51,13 +22,11 @@ public class UIList : MonoBehaviour {
         foreach (var item in items) {
             CreateListItem(item);
         }
-
-        RearrangeItems();
     }
 
     private UIListItem CreateListItem(object item) {
         var listItem = listItemSource.Clone();
-        listItem.transform.parent = _contentTransform;
+        listItem.transform.SetParent(transform, false);
         listItem.transform.localScale = Vector3.one;
 
         var rectTransform = listItem.transform as RectTransform;
@@ -65,30 +34,5 @@ public class UIList : MonoBehaviour {
         listItem.UpdateItem(item);
         _listItems.Add(listItem);
         return listItem;
-    }
-
-    private void RearrangeItems() {
-        for (int i = 0; i < _listItems.Count; i ++) {
-            var listItem = _listItems[i];
-            var rectTransform = listItem.transform as RectTransform;
-
-            var newPosition = new Vector2();
-
-            if (_scrollRect.horizontal) {
-                newPosition.x = i * _cellSize.x - (_listItems.Count * _cellSize.x * 1.0f / 2) + _cellSize.x * 1.0f / 2;
-            } else if (_scrollRect.vertical) {
-                newPosition.y = i * _cellSize.y - (_listItems.Count * _cellSize.y * 1.0f / 2) + _cellSize.y * 1.0f / 2;
-            }
-
-            rectTransform.localPosition = new Vector3(newPosition.x, newPosition.y, 0);
-        }
-
-        if (_scrollRect.horizontal) {
-            _contentTransform.sizeDelta = new Vector2(_cellSize.x * _listItems.Count, _cellSize.y);
-            _contentTransform.localPosition = new Vector3((_listItems.Count * _cellSize.x * 1.0f / 2) + _cellSize.x * 1.0f / 2, 0);
-        } else if (_scrollRect.vertical) {
-            _contentTransform.sizeDelta = new Vector2(_cellSize.x, _cellSize.y * _listItems.Count);
-            _contentTransform.localPosition = new Vector3(0, (_listItems.Count * _cellSize.x * 1.0f / 2) + _cellSize.x * 1.0f / 2);
-        }
     }
 }
