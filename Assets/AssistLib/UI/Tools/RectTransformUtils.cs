d0ef7d3609daa464bf63108ff0f8085a99cd1b55
c1tr00z <0range.br4in@gmail.com>
2019-09-16 22:23:42 +0900

    rect transform utils

---
+++ Assets/AssistLib/UI/Tools/RectTransformUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace c1tr00z.AssistLib.UI {
+    public static class RectTransformUtils {
+        
+        public static void Stretch(this RectTransform rectTransform) {
+            rectTransform.localScale = Vector3.one;
+            rectTransform.rect.Set(0, 0, 0, 0);
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+            rectTransform.offsetMin = Vector2.zero;
+            rectTransform.offsetMax = Vector2.zero;
+        }
+    }
+}
