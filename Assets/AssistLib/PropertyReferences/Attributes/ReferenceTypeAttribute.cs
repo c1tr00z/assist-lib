13949bf6a2f0c12e6313dd40a2c6169a0286e643
c1tr00z <0range.br4in@gmail.com>
2019-09-22 11:05:08 +0900

    property references init commit

---
+++ Assets/AssistLib/PropertyReferences/Attributes/ReferenceTypeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReferenceTypeAttribute : System.Attribute {
+
+    public System.Type type { get; private set; }
+
+    public ReferenceTypeAttribute(System.Type type) {
+        this.type = type;
+    }
+
+    public override bool Match(object obj) {
+        var other = obj as ReferenceTypeAttribute;
+        if (other == null) {
+            return false;
+        }
+
+        return other.type == this.type;
+    }
+}
---
+++ Assets/AssistLib/PropertyReferences/PropertyReference.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace c1tr00z.AssistLib.FieldReferences {
+    [System.Serializable]
+    public class PropertyReference {
+
+        public GameObject reference;
+
+        public string referenceString;
+
+
+    }
+}
