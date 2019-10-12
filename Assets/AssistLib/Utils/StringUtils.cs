c46efa7b83955deae0123697770f2b99673c10c7
c1tr00z <0range.br4in@gmail.com>
2019-10-12 12:58:24 +0900

    Generate Random String Utils Method

--- Assets/AssistLib/Utils/StringUtils.cs
+++ Assets/AssistLib/Utils/StringUtils.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Globalization;
-using System.Text;
+﻿using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
-using UnityEngine;
+using Random = System.Random;
 
 namespace c1tr00z.AssistLib.Localization {
     public static class StringUtils {
+        private static Random random = new Random();
 
         public static string DecodeEncodedNonAscii(this string value) {
+            return Regex.Replace(value, @"\\u(?<Value>[a-zA-Z0-9]{4})",
+                m => {
+                    return ((char) short.Parse(m.Groups["Value"].Value, NumberStyles.AllowHexSpecifier)).ToString();
+                });
+        }
 
-            return Regex.Replace(value, @"\\u(?<Value>[a-zA-Z0-9]{4})", m => {
-                return ((char)Int16.Parse(m.Groups["Value"].Value, NumberStyles.AllowHexSpecifier)).ToString();
-            });
+        public static string RandomString(int length, bool useSpaces = false, bool useNumbers = false) {
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (useSpaces) {
+                chars += "  ";
+            }
+
+            if (useNumbers) {
+                chars += "0123456789";
+            }
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
---
+++ Assets/Examples/Others/DebugLogGeneratedString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using c1tr00z.AssistLib.Localization;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+public class DebugLogGeneratedString : MonoBehaviour {
+    [SerializeField] private int _count = 5;
+    private void Start() {
+        for (int i = 0; i < _count; i++) {
+            Debug.Log(StringUtils.RandomString(Random.Range(5, 25), true, false));
+        }
+    }
+}
