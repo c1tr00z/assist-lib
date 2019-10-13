fcdd6a234a71463b22e56cfd171aaf795275abca
c1tr00z <0range.br4in@gmail.com>
2019-10-13 14:21:53 +0900

    Value Receiver Text

---
+++ Assets/AssistLib/DataModels/ValueReceivers/ValueReceiverText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using c1tr00z.AssistLib.PropertyReferences;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace c1tr00z.AssistLib.DataModels {
+    [RequireComponent(typeof(Text))]
+    public class ValueReceiverText : ValueReceiverBase {
+        [ReferenceType(typeof(string))]
+        [SerializeField]
+        private PropertyReference _textSource;
+
+        private Text _text;
+
+        public Text text {
+            get {
+                if (_text == null) {
+                    _text = GetComponent<Text>();
+                }
+
+                return _text;
+            }
+        }
+
+        public override void UpdateReceiver() {
+            text.text = _textSource.Get<string>();
+        }
+        
+        public override IEnumerator<PropertyReference> GetReferences() {
+            yield return _textSource;
+        }
+    }
+}
