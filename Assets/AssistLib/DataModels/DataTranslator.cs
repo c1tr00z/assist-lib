c651bfa62d97414e6ca1d48dea21b2b591401685
c1tr00z <0range.br4in@gmail.com>
2019-11-16 14:47:51 +0900

    Data Translator

---
+++ Assets/AssistLib/DataModels/DataTranslator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using c1tr00z.AssistLib.PropertyReferences;
+
+namespace c1tr00z.AssistLib.DataModels {
+    public abstract class DataTranslator : DataModelBase, IValueReceiver {
+
+        #region IValueReceiver Implementation
+
+        public abstract void UpdateReceiver();
+
+        public IEnumerable<IDataModelBase> GetModels() {
+            var modelsList = new List<IDataModelBase>();
+            var references = GetReferences();
+            while (references.MoveNext()) {
+                var reference = references.Current;
+                var targetComponent = reference.GetTargetComponent();
+                if (targetComponent is IDataModelBase) {
+                    modelsList.Add(targetComponent as IDataModelBase);
+                }
+            }
+            return modelsList;
+        }
+
+        #endregion
+
+        #region Abstract Methods
+
+        public abstract IEnumerator<PropertyReference> GetReferences();
+
+        #endregion
+    }
+}
