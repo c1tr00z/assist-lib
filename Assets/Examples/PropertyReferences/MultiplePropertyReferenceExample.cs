14c59a88cd5a5ce478d2f8e94215fa96e2f3d23b
c1tr00z <0range.br4in@gmail.com>
2019-12-01 13:11:09 +0900

    examples

---
+++ Assets/Examples/PropertyReferences/MultiplePropertyReferenceExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using c1tr00z.AssistLib.DataModels;
+using c1tr00z.AssistLib.Localization;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace c1tr00z.AssistLib.PropertyReferences {
+    public class MultiplePropertyReferenceExample : DataModelBase {
+        public string string1 { get; private set; }
+        public string string2 { get; private set; }
+        public string string3 { get; private set; }
+        public string string4 { get; private set; }
+        public string string5 { get; private set; }
+
+        private void Start() {
+            Refresh();
+        }
+
+        private void Update() {
+            if (Input.GetKeyUp(KeyCode.R)) {
+                Refresh();
+            }
+        }
+
+        private void Refresh() {
+            string1 = StringUtils.RandomString(Random.Range(5, 25));
+            string2 = StringUtils.RandomString(Random.Range(5, 25));
+            string3 = StringUtils.RandomString(Random.Range(5, 25));
+            string4 = StringUtils.RandomString(Random.Range(5, 25));
+            string5 = StringUtils.RandomString(Random.Range(5, 25));
+            OnDataChanged();
+        }
+    }
+}
---
+++ Assets/Examples/PropertyReferences/SimpleTextSource.cs
@@ -0,0 +1,24 @@
+﻿using c1tr00z.AssistLib.DataModels;
+using c1tr00z.AssistLib.Localization;
+using UnityEngine;
+
+namespace c1tr00z.AssistLib.PropertyReferences {
+    public class SimpleTextSource : DataModelBase {
+        public string text { get; private set; }
+        
+        private void Start() {
+            Refresh();
+        }
+
+        private void Update() {
+            if (Input.GetKeyUp(KeyCode.R)) {
+                Refresh();
+            }
+        }
+        
+        private void Refresh() {
+            text = StringUtils.RandomString(Random.Range(5, 25));
+            OnDataChanged();
+        }
+    }
+}
