0c5370d7089fb95f7caa576a5f62a518d3cf7ad7
c1tr00z <0range.br4in@gmail.com>
2019-12-01 17:33:05 +0900

    property drawer optimization

--- Assets/AssistLib/PropertyReferences/Editor/PropertyReferenceDrawer.cs
+++ Assets/AssistLib/PropertyReferences/Editor/PropertyReferenceDrawer.cs
@@ -14,165 +14,172 @@ namespace c1tr00z.AssistLib.PropertyReferences.Editor {
         private static readonly string FIELD_COMPONENT_INDEX = "componentIndex";
         private static readonly string FIELD_FIELD_NAME = "fieldName";
 
-        private SerializedProperty _property;
-        private SerializedProperty _targetRefProperty;
-        private SerializedProperty _componentTypeProperty;
-        private SerializedProperty _componentIndexProperty;
-        private SerializedProperty _propertyNameProperty;
+        private class PropertyData {
+	        public string property;
+	        public SerializedProperty targetRefProperty;
+	        public SerializedProperty componentTypeProperty;
+	        public SerializedProperty componentIndexProperty;
+	        public SerializedProperty propertyNameProperty;
         
-        private GameObject _targetGameObject;
-        private List<Component> _allComponents;
-        private Dictionary<System.Type, List<Component>> _componentsByType;
-        private string[] _displayedCoomponents;
-        private Type _selectedType;
-        private List<PropertyInfo> _selectedTypeProperties = new List<PropertyInfo>();
-        private string[] _propertiesByType;
+	        public GameObject targetGameObject;
+	        public List<Component> allComponents;
+	        public Dictionary<System.Type, List<Component>> componentsByType;
+	        public string[] displayedCoomponents;
+	        public Type selectedType;
+	        public List<PropertyInfo> selectedTypeProperties = new List<PropertyInfo>();
+	        public string[] propertiesByType;
+        }
+
+        private string _prevProperty;
+        
+        private Dictionary<string, PropertyData> _propertiesData = new Dictionary<string, PropertyData>();
 
         public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
 			EditorGUI.BeginProperty(position, label, property);
 
-			var propertyChanged = false;
-			if (_property != property) {
-				propertyChanged = true;
-				_property = property;
+			if (!_propertiesData.ContainsKey(property.propertyPath)) {
+				_propertiesData.AddOrSet(property.propertyPath, new PropertyData {
+					property = property.propertyPath,
+					targetRefProperty = property.FindPropertyRelative(FIELD_TARGET_OBJECT),
+					componentTypeProperty = property.FindPropertyRelative(FIELD_COMPONENT_TYPE),
+					componentIndexProperty = property.FindPropertyRelative(FIELD_COMPONENT_INDEX),
+					propertyNameProperty = property.FindPropertyRelative(FIELD_FIELD_NAME),
+				});
 			}
 
-			if (propertyChanged) {
-				_targetRefProperty = property.FindPropertyRelative(FIELD_TARGET_OBJECT);
-				_componentTypeProperty = property.FindPropertyRelative(FIELD_COMPONENT_TYPE);
-				_componentIndexProperty = property.FindPropertyRelative(FIELD_COMPONENT_INDEX);
-				_propertyNameProperty = property.FindPropertyRelative(FIELD_FIELD_NAME);
-			}
+			var data = _propertiesData[property.propertyPath];
 
 			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
 
             var targetRefRect = new Rect(position.x, position.y, position.width, 16);
             
-            EditorGUI.PropertyField(targetRefRect, _targetRefProperty, GUIContent.none);
+            EditorGUI.PropertyField(targetRefRect, data.targetRefProperty, GUIContent.none);
 
-            if (_targetRefProperty.objectReferenceValue == null) {
+            if (data.targetRefProperty.objectReferenceValue == null) {
                 return;
             }
 
-            var gameObject = _targetRefProperty.objectReferenceValue as GameObject;
+            var gameObject = data.targetRefProperty.objectReferenceValue as GameObject;
 
             if (gameObject == null) {
                 return;
             }
 
-            var gameObjectChanged = _targetGameObject != gameObject;
+            var gameObjectChanged = data.targetGameObject == null || (_prevProperty == data.property && data.targetGameObject != gameObject);
 
             if (gameObjectChanged) {
-	            _allComponents = gameObject.GetComponents<Component>().ToList();
+	            data.allComponents = gameObject.GetComponents<Component>().ToList();
 	            
-	            _componentsByType = new Dictionary<System.Type, List<Component>>();
-	            _allComponents.ForEach(c => {
+	            data.componentsByType = new Dictionary<System.Type, List<Component>>();
+	            data.allComponents.ForEach(c => {
 		            var type = c.GetType();
-		            if (_componentsByType.ContainsKey(type)) {
-			            _componentsByType[type].Add(c);
+		            if (data.componentsByType.ContainsKey(type)) {
+			            data.componentsByType[type].Add(c);
 		            } else {
 			            var componentsList = new List<Component>();
 			            componentsList.Add(c);
-			            _componentsByType.Add(type, componentsList);
+			            data.componentsByType.Add(type, componentsList);
 		            }
 	            });
 
-	            _displayedCoomponents = _allComponents.Select(c =>
-		            $"{c.GetType().Name}[{_componentsByType[c.GetType()].IndexOf(c)}]").ToArray();
+	            data.displayedCoomponents = data.allComponents.Select(c =>
+		            $"{c.GetType().Name}[{data.componentsByType[c.GetType()].IndexOf(c)}]").ToArray();
 	            
-	            _targetGameObject = gameObject;
+	            data.targetGameObject = gameObject;
             }
 
-            if (_allComponents.Count == 0) {
+            if (data.allComponents.Count == 0) {
                 return;
             }
 
             var selectedTypeChanged = false;
-            if (gameObjectChanged || (_selectedType == null && _componentTypeProperty != null && !string.IsNullOrEmpty(_componentTypeProperty.stringValue))) {
-	            var savedType = ReflectionUtils.GetTypeByName(_componentTypeProperty.stringValue);
-				if (savedType != _selectedType) {
-					_selectedType = savedType;
+            if (gameObjectChanged || (data.selectedType == null && data.componentTypeProperty != null && !string.IsNullOrEmpty(data.componentTypeProperty.stringValue))) {
+	            var savedType = ReflectionUtils.GetTypeByName(data.componentTypeProperty.stringValue);
+				if (savedType != data.selectedType) {
+					data.selectedType = savedType;
 					selectedTypeChanged = true;
 				}
             }
             
-            if (_selectedType == null || !_componentsByType.ContainsKey(_selectedType)) {
-	            _selectedType = _allComponents.First().GetType();
+            if (data.selectedType == null || !data.componentsByType.ContainsKey(data.selectedType)) {
+	            data.selectedType = data.allComponents.First().GetType();
 	            selectedTypeChanged = true;
             }
 
             var componentsPopupRect = new Rect(position.x, position.y + 20, position.width, 16);
             
-			var selectedComponentIndex = _componentIndexProperty.intValue;
+			var selectedComponentIndex = data.componentIndexProperty.intValue;
 
-            selectedComponentIndex = selectedComponentIndex < _componentsByType[_selectedType].Count ? selectedComponentIndex : 0;
-			var selectedComponent = _componentsByType[_selectedType][selectedComponentIndex];
-			var selectedTypeIndex = _allComponents.IndexOf(selectedComponent);
+            selectedComponentIndex = selectedComponentIndex < data.componentsByType[data.selectedType].Count ? selectedComponentIndex : 0;
+			var selectedComponent = data.componentsByType[data.selectedType][selectedComponentIndex];
+			var selectedTypeIndex = data.allComponents.IndexOf(selectedComponent);
 
 			selectedTypeIndex = EditorGUI.Popup(componentsPopupRect, selectedTypeIndex, 
-				_displayedCoomponents);
+				data.displayedCoomponents);
 
-			selectedComponent = _allComponents[selectedTypeIndex];
+			selectedComponent = data.allComponents[selectedTypeIndex];
 			var newSelectedType = selectedComponent.GetType();
 
-			if (_selectedType != newSelectedType) {
-				_selectedType = newSelectedType;
+			if (data.selectedType != newSelectedType) {
+				data.selectedType = newSelectedType;
 				selectedTypeChanged = true;
 			}
 			
-			selectedComponentIndex = _componentsByType[_selectedType].IndexOf(selectedComponent);
+			selectedComponentIndex = data.componentsByType[data.selectedType].IndexOf(selectedComponent);
 
-			_componentTypeProperty.stringValue = _selectedType.FullName;
-            _componentIndexProperty.intValue = selectedComponentIndex;
+			data.componentTypeProperty.stringValue = data.selectedType.FullName;
+			data.componentIndexProperty.intValue = selectedComponentIndex;
 
-            if (selectedTypeChanged || _selectedTypeProperties.Count == 0) {
-	            _selectedTypeProperties = _selectedType.GetPublicProperties().SelectNotNull().ToList();
+            if (selectedTypeChanged || data.selectedTypeProperties.Count == 0) {
+	            data.selectedTypeProperties = data.selectedType.GetPublicProperties().SelectNotNull().ToList();
 	            
-	            if (_selectedTypeProperties.Count == 0) {
+	            if (data.selectedTypeProperties.Count == 0) {
 		            return;
 	            }
 
 	            var drawerAttribute = attribute as ReferenceTypeAttribute;
 
 	            if (drawerAttribute != null) {
-		            var list = _selectedTypeProperties;
-		            _selectedTypeProperties = new List<PropertyInfo>();
-		            _selectedTypeProperties.AddRange(list.Where(p => p.PropertyType == drawerAttribute.type));
+		            var list = data.selectedTypeProperties;
+		            data.selectedTypeProperties = new List<PropertyInfo>();
+		            data.selectedTypeProperties.AddRange(list.Where(p => p.PropertyType == drawerAttribute.type));
 		            if (drawerAttribute.type == typeof(string)) {
-			            _selectedTypeProperties.AddRange(list.Where(p => p.PropertyType != drawerAttribute.type));
+			            data.selectedTypeProperties.AddRange(list.Where(p => p.PropertyType != drawerAttribute.type));
 		            }
 		            else {
-			            _selectedTypeProperties.AddRange(list.Where(p => p.PropertyType.IsSubclassOf(drawerAttribute.type)));
-			            _selectedTypeProperties.AddRange(list.Where(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition() == drawerAttribute.type));
+			            data.selectedTypeProperties.AddRange(list.Where(p => p.PropertyType.IsSubclassOf(drawerAttribute.type)));
+			            data.selectedTypeProperties.AddRange(list.Where(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition() == drawerAttribute.type));
 		            }
 				
 		            if (drawerAttribute.type.IsSubclassOf(typeof(string))) {
-			            _selectedTypeProperties.AddRange(list.Where(p => !p.PropertyType.IsSubclassOf(drawerAttribute.type)));
+			            data.selectedTypeProperties.AddRange(list.Where(p => !p.PropertyType.IsSubclassOf(drawerAttribute.type)));
 		            }
 	            }
 	            
-	            _propertiesByType = _selectedTypeProperties.Select(p => p.GetPropertyNameByType()).ToArray();
+	            data.propertiesByType = data.selectedTypeProperties.Select(p => p.GetPropertyNameByType()).ToArray();
             }
 			
-            var selectedProperty = _propertyNameProperty == null 
-                || string.IsNullOrEmpty(_propertyNameProperty.stringValue) 
-                || _selectedTypeProperties.Select(p => p.Name == _propertyNameProperty.stringValue).Count() == 0
-                ? _selectedTypeProperties.First() 
-                : _selectedTypeProperties.Where(f => f.Name == _propertyNameProperty.stringValue).First();
+            var selectedProperty = data.propertyNameProperty == null 
+                || string.IsNullOrEmpty(data.propertyNameProperty.stringValue) 
+                || data.selectedTypeProperties.Select(p => p.Name == data.propertyNameProperty.stringValue).Count() == 0
+                ? data.selectedTypeProperties.First() 
+                : data.selectedTypeProperties.Where(f => f.Name == data.propertyNameProperty.stringValue).First();
 
 			if (selectedProperty == null) {
-				selectedProperty = _selectedTypeProperties.First();
+				selectedProperty = data.selectedTypeProperties.First();
 			}
 
             if (selectedProperty == null) {
                 return;
             }
 
-            var selectedFieldIndex = _propertiesByType.IndexOf(selectedProperty.GetPropertyNameByType());
+            var selectedFieldIndex = data.propertiesByType.IndexOf(selectedProperty.GetPropertyNameByType());
 
             var fieldPopupRect = new Rect(position.x, position.y + 40, position.width, 16);
-            selectedFieldIndex = EditorGUI.Popup(fieldPopupRect, selectedFieldIndex, _propertiesByType);
-            _propertyNameProperty.stringValue = _selectedTypeProperties[selectedFieldIndex].Name;
+            selectedFieldIndex = EditorGUI.Popup(fieldPopupRect, selectedFieldIndex, data.propertiesByType);
+            data.propertyNameProperty.stringValue = data.selectedTypeProperties[selectedFieldIndex].Name;
+
+            _prevProperty = data.property;
 
             EditorGUI.EndProperty();
 		}
