5a4a5a98de324876f8085fbddfa440ae8e49b6dc
c1tr00z <0range.br4in@gmail.com>
2019-12-08 14:39:34 +0900

    settings

---
+++ Assets/AssistLib/DB/Editor/DBSettings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using c1tr00z.AssistLib.EditorTools;
+using UnityEditor;
+using UnityEngine;
+
+namespace c1tr00z.AssistLib.DataBase.Editor {
+    [EditorToolName("DB Settings")]
+    public class DBSettings : EditorTool {
+        [Serializable]
+        public class SaveData {
+            public bool autoCollect;
+        }
+
+        private static string key => typeof(DBSettings).FullName;
+
+
+        private SaveData _saveData;
+
+        public static bool autoCollect => LoadData().autoCollect;
+
+        public override void Init(Dictionary<string, object> settings) {
+            base.Init(settings);
+            Load();
+        }
+
+        public override void Save(Dictionary<string, object> settings) {
+            base.Save(settings);
+            Save();
+        }
+
+        protected override void DrawInterface() {
+            _saveData.autoCollect = EditorGUILayout.Toggle("Use autocollect", _saveData.autoCollect);
+        }
+
+        private static SaveData LoadData() {
+            var json = EditorPrefs.GetString(key);
+            return string.IsNullOrEmpty(json) ? new SaveData() : JsonUtility.FromJson<SaveData>(json);
+        }
+
+        private void Load() {
+            _saveData = LoadData();
+        }
+
+        private void Save() {
+            var json = JsonUtility.ToJson(_saveData);
+            EditorPrefs.SetString(key, json);
+        }
+    }
+}
