df0c7a9cdd58c08d33336e014dd6e043fc0eb9f6
c1tr00z <0range.br4in@gmail.com>
2019-12-09 21:10:11 +0900

    Property reference fixes

--- Assets/AssistLib/PropertyReferences/PropertyReferenceUtils.cs
+++ Assets/AssistLib/PropertyReferences/PropertyReferenceUtils.cs
@@ -1,28 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 namespace c1tr00z.AssistLib.PropertyReferences {
     public static class PropertyReferenceUtils {
+        
+        private static Dictionary<string, PropertyInfo> _properties = new Dictionary<string, PropertyInfo>();
 
         public static T Get<T>(this PropertyReference propertyReference) {
-            var component = propertyReference.GetTargetComponent();
-            var type = component.GetType();
-            var field = type.GetPublicPropertyInfo(propertyReference.fieldName);
-            if (field == null) {
+            if (propertyReference.field == null) {
+                var key = propertyReference.GetPropertyKey();
+                propertyReference.component = propertyReference.GetTargetComponent();
+                if (!_properties.ContainsKey(key)) {
+                    var type = propertyReference.component.GetType();
+                    _properties.AddOrSet(key, type.GetPublicPropertyInfo(propertyReference.fieldName));
+                }
+
+                propertyReference.field = _properties[key];
+
+            }
+            if (propertyReference.field == null) {
                 return default(T);
             }
+
+            var value = propertyReference.GetValue();
             if (typeof(T) == typeof(string)) {
-                if (field.GetValue(component, null) == null) {
+                if (value == null) {
                     return default(T);
                 }
-                return (T)(object)field.GetValue(component, null).ToString();
+                return (T)(object)propertyReference.GetValue().ToString();
             }
-            var value = field.GetValue(component, null);
             if (value is T) {
                 return (T)value;
             }
             return default(T);
+        }
+
+        private static string GetPropertyKey(this PropertyReference propertyReference) {
+            return $"{propertyReference.targetComponentTypeName}.{propertyReference.fieldName}";
+        }
+
+        public static object GetValue(this PropertyReference propertyReference) {
+            return propertyReference.field.GetValue(propertyReference.component, null);
         }
 
         public static Component GetTargetComponent(this PropertyReference propertyReference) {
